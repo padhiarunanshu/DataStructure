9406a9e56daeca2f02cd42e81bc7558f5cde0fe5
Arunanshu <31306851+padhiarunanshu@users.noreply.github.com>
2020-08-14 01:03:35 +0530

    Create PrefixSortingProblem.cpp

---
+++ Algorithms/PrefixSortingProblem.cpp
@@ -0,0 +1,5 @@
+/**
+ * Approach: 
+    Find the largest element. then do a prefix sort, then reverse the array.
+    Find the largest element in array of size n - 1 (largest is stored at correct position), and continue the previous step. 
+ */
