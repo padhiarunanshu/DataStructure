700ae15e9bb72c8be94428624d01c835e6e90c1a
apadhi <padhiarunanshu@gmail.com>
2022-05-09 21:35:00 +0530

    Minor code update

--- Graph/DirectedGraphUsingSTL.cpp
+++ Graph/DirectedGraphUsingSTL.cpp
@@ -4,10 +4,7 @@
 #include <vector>
 using namespace std;
 
-// data structure to store graph edges
-struct Edge {
-	int src, dest;
-};
+using Edge = vector<int> ;
 
 // class to represent a graph object
 class Graph
@@ -17,7 +14,7 @@ public:
 	vector<vector<int>> adjList;
 
 	// Graph Constructor
-	Graph(vector<Edge> const &edges, int N)
+	Graph(const vector<Edge> &edges, int N)
 	{
 		// resize the vector to N elements of type vector<int>
 		adjList.resize(N);
@@ -26,16 +23,16 @@ public:
 		for (auto &edge: edges)
 		{
 			// insert at the end
-			adjList[edge.src].push_back(edge.dest);
+			adjList[edge[0]].push_back(edge[1]);
 
 			// Uncomment below line for undirected graph
-			// adjList[edge.dest].push_back(edge.src);
+			adjList[edge[1]].push_back(edge[0]);
 		}
 	}
 };
 
 // print adjacency list representation of graph
-void printGraph(Graph const& graph, int N)
+void printGraph(const Graph& graph, int N)
 {
 	for (int i = 0; i < N; i++)
 	{
@@ -53,12 +50,8 @@ void printGraph(Graph const& graph, int N)
 int main()
 {
 	// vector of graph edges as per above diagram.
-	// Please note that initialization vector in below format will
-	// work fine in C++11, C++14, C++17 but will fail in C++98.
-	vector<Edge> edges =
-	{
-		{ 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
-		{ 3, 2 }, { 4, 5 }, { 5, 4 }
+	vector<Edge> edges = {
+		{0, 1}, {1, 2}, { 2, 0 }, { 2, 1 },{ 3, 2 }, { 4, 5 }, { 5, 4 }
 	};
 
 	// Number of nodes in the graph
